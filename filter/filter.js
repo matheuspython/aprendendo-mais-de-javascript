b265d6f103f2f730f1e80cac39c88a1a4c431a98
matheus souza <ruivorobado@gmail.com>
2021-01-26 14:56:04 -0300

    exemplo sem filter

---
+++ filter/filter.js
@@ -0,0 +1,9 @@
+/*como posso retornar os numeros maiores que 10 desse array?
+
+const numeros = [5, 50, 80, 10, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+let numerosMaioresQue10 = []
+
+for (numero of numeros) { if (numero > 10) numerosMaioresQue10.push(numero) }
+
+eu particularmente faria dessa forma se fosse sem utilizar filter
+*/
