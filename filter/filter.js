3a03f0c38deababe41cd78e814b58c3c7c9e5318
matheus souza <ruivorobado@gmail.com>
2021-01-26 15:10:17 -0300

    exemplo com filter

--- filter/filter.js
+++ filter/filter.js
@@ -7,3 +7,7 @@ for (numero of numeros) { if (numero > 10) numerosMaioresQue10.push(numero) }
 
 eu particularmente faria dessa forma se fosse sem utilizar filter
 */
+const numeros = [5, 50, 80, 10, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+const numerosFiltrados = numeros.filter(valor => valor > 10)
+
+console.log(numerosFiltrados)
